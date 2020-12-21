21102235abd31a17a92cf1f0f98c128dd38a3d29
Aleks Pickle <aleks.work2222@gmail.com>
2020-12-21 15:40:00 +0200

    fix paths

--- scripts/v2.sh
+++ scripts/v2.sh
@@ -2,5 +2,5 @@ n=2
 port=30334
 ws_port=9946
 rpc_port=9934
-../node/target/release/node-template --chain ./chainSpecRaw.json --name node-n$n --port $port --ws-port $ws_port --rpc-port $rpc_port --base-path /tmp/nodes/n$n/data/  --rpc-methods Unsafe --validator --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWJEqTkfY5EGV2KwuFEUzBE1b2CsCJ6aRkqMuioHuJjD4Z 
+../node/target/release/node-template --chain ./chainSpecRaw.json --name node-n$n --port $port --ws-port $ws_port --rpc-port $rpc_port --base-path /tmp/nodes/n$n/data/  --rpc-methods Unsafe --validator --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWLWGegAqdQPGHjSTML4uXM7TC9spGhbWDaELoan8SmoXm
 
